2776d877676230e5eaf46bd59791d638014f9a8a
AlexandraRuzina <Alexandra.Ruzina@Student.HTW-Berlin.de>
2025-11-23 16:06:26 +0100

    Daten werden gefetcht

---
+++ experiments/exp_1_1/scripts/01_data_acquisition/01_data_acquisition.py
@@ -0,0 +1,33 @@
+from alpaca.data.historical import CryptoHistoricalDataClient
+from alpaca.data.requests import CryptoBarsRequest
+from alpaca.data.timeframe import TimeFrame
+import pandas as pd
+from datetime import datetime
+import pytz
+import yaml
+
+# Load API credentials from YAML configuration file
+keys = yaml.safe_load(open("../../conf/keys.yaml"))
+API_KEY = keys['KEYS']['APCA-API-KEY-ID-Data']
+SECRET_KEY = keys['KEYS']['APCA-API-SECRET-KEY-Data']
+
+# Load data acquisition parameters from YAML configuration file
+params = yaml.safe_load(open("../../conf/params.yaml"))
+PATH_BARS = params['DATA_ACQUISITION']['DATA_PATH']
+start_date = datetime.strptime(params['DATA_ACQUISITION']['START_DATE'], "%Y-%m-%d")
+end_date = datetime.strptime(params['DATA_ACQUISITION']['END_DATE'], "%Y-%m-%d")
+symbols = params["DATA_ACQUISITION"]["SYMBOLS"]
+
+# Initialize the Alpaca client with API credentials
+crypto_client = CryptoHistoricalDataClient(api_key=API_KEY, secret_key=SECRET_KEY)
+
+for symbol in symbols:
+    request = CryptoBarsRequest(
+        symbol_or_symbols=symbol,
+        timeframe=TimeFrame.Hour,
+        start=start_date,
+        end=end_date
+    )
+
+    bars = crypto_client.get_crypto_bars(request)
+    df = bars.df
